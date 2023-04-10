d473bc63a3e6877af941d297e74dc816974f76d6
yaroslav <yaroslav@>
2023-04-10 15:12:41 +0300

    add telegram token

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,24 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var apiKey = os.Getenv("TELEGRAM_KEY")
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	bot, err := tgbotapi.NewBotAPI(apiKey)
 	if err != nil {
 		log.Panic(err)
 	}
 
 	bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	//log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
 
-	updates := bot.GetUpdatesChan(u)
+	updates := bot.GetUpdatesChan(updateConfig)
 
 	for update := range updates {
 		if update.Message != nil { // If we got a message
